4e65e5d215bc0b52cc49e1af764e839f72c409bc
Aviva1234 <155398801+Aviva1234@users.noreply.github.com>
2024-08-17 13:23:42 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -3,6 +3,13 @@ init:function(){
     numTurtles = 5;
     this.el.addEventListener('collide',function(e){
 numTurtles--;
+oldNumber = document.getElementById("numTurtles");
+oldNumber.setAttribute("text", {value: String(numTurtles)});
+e.detail.target.el.remove();
+winMessage = document.getElementById("winMessage");
+if (numTurtles == 0){
+winMessage.setAttribute("text", {value: "Yay!\nYou Win"})
+}
     });
 }
 });
